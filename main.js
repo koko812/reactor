0a4940c585859ea596e371e0ef860f23feb7c217
koko812 <iwakawa.koichi.q5@dc.tohoku.ac.jp>
2025-04-28 02:25:10 +0900

    add_comments

--- main.js
+++ main.js
@@ -9,10 +9,21 @@ class Character {
         element.style.position = 'absolute'
         element.style.height = `${size}px`
         element.style.width = `${size}px`
+        // ごめんなさい，この display flex も普通に謎です
+        // 一番最初に tetoris を作ろうとしたときにこんなのをつけた記憶がなくもないが
+        // グリッドレイアウトな何かを作ろうとし出したら，分かってくるかもしれない
+        // それっぽいブログみたいなホームページを今度作るときは，今までよりもクオリティを上げられそうで，
+        // その時にこのあたりの機能を色々使うと思うので，その時にきちんと押さえればOKだと思われる
         element.style.display = 'flex'
+        // この下二つがマジで謎なんだが，とにかく行，列の観点でセンタリングしたいらしい
+        // なぜ Items と Content で振り分けるのかがマジで謎なんだが，
+        // それは god only knows ...
         element.style.alignItems = 'center'
         element.style.justifyContent = 'center'
         element.style.fontSize = `${size * 0.9}px`
+        // この color の設定も謎だが，これはフォントのカラーのことを言っている？
+        // 流石にそんなことはないと信じたいが，よくわからない？
+        // 今回はひよこを置いてるから黄色くなってるだけで，矢の色などはこの色に依存するのかな多分
         element.style.color = '#fff'
         element.textContent = text
     }
